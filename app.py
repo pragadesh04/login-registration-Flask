7a80038772e17b6b5a44e2325ad66e97f1333ef4
Praga866 <pragaofficial004@gmail.com>
2024-08-10 14:44:29 +0530

    included rough Hashing

--- app.py
+++ app.py
@@ -1,6 +1,39 @@
 from flask import Flask, render_template, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+def hashing(password):
+    hash = ""
+    specialChars = {
+        "!":"1001",
+        "@":"1011",
+        "#":"1111",
+        "$":"1002",
+        "%":"1022",
+        "^":"1222",
+        "&":"2222",
+        "*":"2003",
+        "(":"2033",
+        ")":"2333"
+    }
+    for i in range(len(password)):
+        if password[i].isalpha():
+            numbers = str(ord(password[i].lower()) - ord('a') + 1) + "@#" 
+        elif password[i].isdigit():
+            numbers = str(int(password[i]) * int(password[i]))
+        else:
+            numbers = specialChars[password[i]] + "#@"
+        hash += numbers
+    return hash
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///datas.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class data(db.Model):
+    username = db.Column(db.String(20), primary_key = True)
+    password = db.Column(db.String(80), nullable = False)
 
 @app.route("/")
 def index():
@@ -58,11 +91,24 @@ def loadSignIn():
 def signup():
     username = request.form.get('username')
     password = request.form.get('password')
-    confirm_password = request.form.get('new-password')
+    confirm_password = request.form.get('confirm_password')
+    global hash
 
-    print(f"Username: {username}, Password: {password}, Confirm Password: {confirm_password}")
-    return "<h1>SignUp successful</h1>"
+    user = data.query.filter_by(username='user1').first()
 
+    if not user and password == confirm_password:
+        print(f"Username: {username}, Password: {password}, Confirm Password: {confirm_password}")
+
+        hash = hashing(password)
+
+        print(hash)
+        datas = data(username = username, password = hash)
+        db.session.add(datas)
+        db.session.commit()
+    else:
+        return "<h1>Passwords do not match</h1>"
+    return "Successfull"
+    
 @app.route("/signin", methods=['POST'])
 def signin():
     username = request.form.get('username')
@@ -70,6 +116,8 @@ def signin():
 
     print(f"Username: {username}, Password: {password}")
     return "<h1>SignIn successful</h1>"
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
