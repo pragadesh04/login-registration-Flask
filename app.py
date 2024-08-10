1ca1934f4f378e02fd5505be5bd701c043f4b4dd
Praga866 <pragaofficial004@gmail.com>
2024-08-10 17:20:57 +0530

    used functions

--- app.py
+++ app.py
@@ -3,51 +3,30 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# Hashing function
 def hashing(password):
-    hash = ""
     specialChars = {
-        "!":"1001",
-        "@":"1011",
-        "#":"1111",
-        "$":"1002",
-        "%":"1022",
-        "^":"1222",
-        "&":"2222",
-        "*":"2003",
-        "(":"2033",
-        ")":"2333"
+        "!": "1001", "@": "1011", "#": "1111", "$": "1002",
+        "%": "1022", "^": "1222", "&": "2222", "*": "2003",
+        "(": "2033", ")": "2333"
     }
-    for i in range(len(password)):
-        if password[i].isalpha():
-            numbers = str(ord(password[i].lower()) - ord('a') + 1) + "@#" 
-        elif password[i].isdigit():
-            numbers = str(int(password[i]) * int(password[i]))
+    hash = ""
+    for char in password:
+        if char.isalpha():
+            hash += str(ord(char.lower()) - ord('a') + 1) + "@#"
+        elif char.isdigit():
+            hash += str(int(char) * int(char))
         else:
-            numbers = specialChars[password[i]] + "#@"
-        hash += numbers
+            hash += specialChars.get(char, "") + "#@"
     return hash
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///datas.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
-
-class data(db.Model):
-    username = db.Column(db.String(20), primary_key = True)
-    password = db.Column(db.String(80), nullable = False)
-
-@app.route("/")
-def index():
-    return render_template("login.html")
-
-@app.route("/signup-content")
-def loadSignUp():
-    content = """<form action="/signup" method="post">
+def signup():
+    return """<form action="/signup" method="post">
     <div class="innerContainer">
         <div class="content">
             <label for="signup-username">Username</label><br/>
             <input type="text" class="inputs" id="signup-username" name="username" required>
         </div>
-
         <div class="content">
             <label for="signup-password">New Password:</label><br/>
             <input type="password" class="inputs" id="signup-password" name="password" required>
@@ -59,21 +38,17 @@ def loadSignUp():
         <div class="text-center content">
             <button class="button sign" type="submit">SignUp</button>
         </div>
-
         <p class="text-center content">Already Have an Account? <a onclick="loadSignIn()" class="links">Sign-In</a></p>
     </div>
     </form>"""
-    return jsonify({'html': content})
 
-@app.route("/signin-content")
-def loadSignIn():
-    signIn = """<div class="innerContainer">
+def signin():
+    return """<div class="innerContainer">
         <form action="/signin" method="post">
             <div class="content">
                 <label for="signin-username">Username:</label><br/>
                 <input type="text" class="inputs" id="signin-username" name="username" required>
             </div>
-
             <div class="content">
                 <label for="signin-password">Password:</label><br/>
                 <input type="password" class="inputs" id="signin-password" name="password" required>
@@ -81,52 +56,63 @@ def loadSignIn():
             <div class="text-center content">
                 <button class="button sign" type="submit">SignIn</button>
             </div>
-
             <p class="text-center content">Don't Have an Account? <a class="links" onclick="loadSignUp()">Sign-up</a></p>
         </form>
     </div>"""
-    return jsonify({'html': signIn})
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///datas.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    username = db.Column(db.String(20), primary_key=True)
+    password = db.Column(db.String(80), nullable=False)
+
+@app.route("/")
+def index():
+    return render_template("login.html")
+
+@app.route("/signup-content")
+def load_sign_up():
+    return jsonify({'html': signup()})
+
+@app.route("/signin-content")
+def load_sign_in():
+    return jsonify({'html': signin()})
 
 @app.route("/signup", methods=['POST'])
 def signup():
     username = request.form.get('username')
     password = request.form.get('password')
     confirm_password = request.form.get('confirm_password')
-    global hash
 
-    user = data.query.filter_by(username=username).first()
+    user = Data.query.filter_by(username=username).first()
+
+    if len(password) < 8:
+        return render_template('messages.html', message="Provide a stronger password", buttonMsg="Let me Try Again!")
     if user:
-        return render_template('messages.html', message="Name Already exsit Login!", buttonMsg = "Login")
+        return render_template('messages.html', message="Name already exists. Login!", buttonMsg="Login")
+    if password != confirm_password:
+        return render_template('messages.html', message="Passwords do not match", buttonMsg="Try Again")
 
-    if password == confirm_password:
-        print(f"Username: {username}, Password: {password}, Confirm Password: {confirm_password}")
-
-        hash = hashing(password)
-
-        print(hash)
-        datas = data(username = username, password = hash)
-        db.session.add(datas)
-        db.session.commit()
-    else:
-        return render_template('messages.html', message = "Try Again", buttonMsg = "Okay!")
-    return render_template('messages.html', message = "Account Created Succesfully",buttonMsg="Try Again!")
+    hashed_password = hashing(password)
+    new_user = Data(username=username, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
     
+    return render_template('messages.html', message="Account Created Successfully", buttonMsg="Login!")
+
 @app.route("/signin", methods=['POST'])
 def signin():
     username = request.form.get('username')
     password = request.form.get('password')
 
-    user = data.query.filter_by(username = username).first()
+    user = Data.query.filter_by(username=username).first()
 
-    if user:
-        print(user.password)
-        print(hashing(password))
-        if user.password == hashing(password):
-                return render_template('messages.html', message = "Logged in SuccessFully", buttonMsg = "Okay!")     
-    else:
-        return render_template('messages.html', message = "Try Again", buttonMsg="Try Again!")
-
-
+    if user and user.password == hashing(password):
+        return render_template('messages.html', message="Logged in Successfully", buttonMsg="Okay!")
+    
+    return render_template('messages.html', message="Invalid credentials, try again.", buttonMsg="Try Again!")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
